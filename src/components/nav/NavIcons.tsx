743587b8d6c8cf7184f16b8b0dfb00fea043ed9d
vlad.kischenko <vlad5114@gmail.com>
2024-10-23 19:14:37 +0300

    remove NavIcons.tsx

--- src/components/nav/NavIcons.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ArrowUpRight, ChevronRight } from '@/components/Icons';
-
-export const NavIcons = {
-    chevron: <ChevronRight size={14} />,
-    arrowUpRight: <ArrowUpRight size={20} />,
-};
