ef9472c5783af74a5f5d00b25fd229599722c104
vlad.kischenko <vlad5114@gmail.com>
2024-10-28 18:25:46 +0300

    fixed .env db namings

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -14,10 +14,10 @@ interface Database {
 export const db = new Kysely<Database>({
     dialect: new PostgresDialect({
         pool: new Pool({
-            host: process.env.DATABASE_HOST,
-            database: process.env.DATABASE_NAME,
-            user: process.env.DATABASE_USER,
-            password: process.env.DATABASE_PASSWORD,
+            host: process.env.POSTGRES_HOST,
+            database: process.env.POSTGRES_DATABASE,
+            user: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
         }),
     }),
 });
