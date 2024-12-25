83311b98f9f561fe0c6f388698809b09235d0b8b
vlad.kischenko <vlad5114@gmail.com>
2024-12-25 15:05:34 +0300

    styled-landing: fixed ui at tablet

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const API_PREFIX = '/api';
-export const MOBILE_WIDTH_BREAKPOINT = 768;
+export const MOBILE_WIDTH_BREAKPOINT = 770;
 export const HEADER_HEIGHT = 85;
 export const VCF_FILE_EXTENSION = '.vcf';
